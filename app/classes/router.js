e9fdff3b0157c6c28317184868fa7d29ae2c64d9
Xaveer van der Laan <xavlaan@protonmail.com>
2023-12-04 08:44:50 +0100

    WIP - Router

---
+++ app/classes/router.js
@@ -0,0 +1,11 @@
+const http = require("node:http");
+const router = require("find-my-way")();
+const path = require("node:path");
+
+export default class laanRouter {
+  constructor() {}
+
+  forgeDefaultViews() {
+    const viewPath = "asd";
+  }
+}
